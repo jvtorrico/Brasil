0646fcee61cd4fc6f0c44dd933361333ce9a56ee
jvtorrico <103641852+jvtorrico@users.noreply.github.com>
2023-05-19 09:50:07 -0300

    Add files via upload

---
+++ BichoChique/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+          <meta charset="UTF-8">
+        <title>PetShop</title>
+        <link href="css/default.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="container">
+            
+            <form method="post"> 
+            
+            
+            
+            
+            
+            
+            
+            
+            </form>
+            
+            
+            
+        </div>
+        
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
